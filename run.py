98c9883a8c560ef10b5213825d7dc11e8fa8f9db
grace-and-truth <admin@graceandtruth.website>
2025-06-30 22:35:15 +0300

    Update run.py

--- run.py
+++ run.py
@@ -4,8 +4,4 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
-python -m venv venv
-source venv/bin/activate     # on Windows: venv\Scripts\activate
-pip install -r requirements.txt
-python run.py
 
